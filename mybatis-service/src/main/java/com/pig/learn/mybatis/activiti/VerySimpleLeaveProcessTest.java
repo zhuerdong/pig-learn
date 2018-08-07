cfd3c2d36d54d28b2cd50cb4b3bf1a4275779427
pig <zhuerdong123@163.com>
2018-08-08 06:45:59 +0800

    提交代码

---
+++ mybatis-service/src/main/java/com/pig/learn/mybatis/activiti/VerySimpleLeaveProcessTest.java
@@ -0,0 +1,33 @@
+package com.pig.learn.mybatis.activiti;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.runtime.ProcessInstance;
+
+public class VerySimpleLeaveProcessTest {
+    public static void main(String[] args) {
+
+       ProcessEngine engine = ProcessEngineConfiguration.createStandaloneInMemProcessEngineConfiguration().buildProcessEngine();
+
+
+
+        RepositoryService repositoryService = engine.getRepositoryService();
+
+        repositoryService.createDeployment().addClasspathResource("diagrams/leave/leave.bpmn").deploy();
+
+
+        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery().singleResult();
+
+
+        System.out.println(processDefinition.getKey());
+
+        RuntimeService runtimeService = engine.getRuntimeService();
+
+
+        ProcessInstance processInstance = runtimeService.startProcessInstanceById("leave");
+
+    }
+}
